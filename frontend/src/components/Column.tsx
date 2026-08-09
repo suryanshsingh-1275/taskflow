27cc77073633a2bbaf8e7b3b5c7ac0a2b0f321c5
Suryansh Singh <singhsuryansh2746@gmail.com>
2026-08-09 15:34:38 +0530

    updated the column.tsx and added showcreatetask var

--- frontend/src/components/Column.tsx
+++ frontend/src/components/Column.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import TaskCard from "./TaskCard";
+import CreateTaskModal from "./CreateTaskModel";
 
 interface Task {
     id: string;
@@ -14,13 +15,38 @@ interface ColumnProps {
     id: string;
     title: string;
     tasks: Task[];
+    boardId: string;
 }
 
 const Column = ({
     id,
     title,
     tasks,
+    boardId,
 }: ColumnProps) => {
+
+    const [showCreateTask, setShowCreateTask] = useState(false);
+
+    const handleCreateTask = (task: {
+        title: string;
+        description: string;
+        priority: string;
+        dueDate: string;
+        assignee: string;
+    }) => {
+
+        console.log("Creating task...");
+
+        console.log("Board ID:", boardId);
+
+        console.log("Column ID:", id);
+
+        console.log("Task:", task);
+
+        // Later:
+        // Axios POST request will go here.
+    };
+
 
     return (
 
@@ -77,18 +103,32 @@ const Column = ({
 
             <div className="column-footer">
 
-                <button className="add-task-column-button">
-
+                <button
+                    className="add-task-column-button"
+                    onClick={() => setShowCreateTask(true)}
+                >
                     + Add Task
-
                 </button>
 
             </div>
 
+
+            <CreateTaskModal
+
+                isOpen={showCreateTask}
+
+                onClose={() => setShowCreateTask(false)}
+
+                boardId={boardId}
+
+                columnId={id}
+
+                onCreate={handleCreateTask}
+
+            />
+
         </div>
-
     );
-
 };
 
 export default Column;
