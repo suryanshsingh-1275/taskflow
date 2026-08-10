1915f4487bd64762d9d37dff579a7dc6266c308c
Suryansh Singh <singhsuryansh2746@gmail.com>
2026-08-10 20:11:15 +0530

    routes added to authroute

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,4 +1,16 @@
 import express from "express";
+import {
+    signup,
+    login
+} from "../controllers/authController.js";
+
+
+
+router.post("/signup", signup);
+
+
+router.post("/login", login);
+
 
 const router = express.Router();
 
