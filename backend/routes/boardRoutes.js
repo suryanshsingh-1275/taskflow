7a2018e4f0616288a7cf56ffe1576f83775d22a1
Suryansh Singh <singhsuryansh2746@gmail.com>
2026-08-10 20:09:02 +0530

    routes on board routes

--- backend/routes/boardRoutes.js
+++ backend/routes/boardRoutes.js
@@ -1,5 +1,22 @@
 import express from "express";
+import {
+    createBoard,
+    getBoards,
+    getBoard,
+    updateBoard,
+    deleteBoard
+} from "../controllers/boardController.js";
 
 const router = express.Router();
+
+router.post("/", createBoard);
+
+router.get("/", getBoards);
+
+router.get("/:id", getBoard);
+
+router.put("/:id", updateBoard);
+
+router.delete("/:id", deleteBoard);
 
 export default router;
