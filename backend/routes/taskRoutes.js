dd6ee47a6b58048d7f4f00d2544a5d982b196b05
Suryansh Singh <singhsuryansh2746@gmail.com>
2026-08-10 20:36:07 +0530

    added routes to taskroute

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,6 +1,29 @@
 import express from "express";
+import {
+    createTask,
+    getBoardTasks,
+    getTask,
+    updateTask,
+    deleteTask
+} from "../controllers/taskController.js";
+
+
+
 
 const router = express.Router();
+
+router.post("/", createTask);
+
+router.get("/board/:boardId", getBoardTasks);
+
+router.get("/:id", getTask);
+
+router.put("/:id", updateTask);
+
+router.delete("/:id", deleteTask);
+
+
+
 
 export default router;
 
