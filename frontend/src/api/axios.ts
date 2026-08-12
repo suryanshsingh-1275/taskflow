f9c8f4d279589ceaba05cb9fd56e4e9afa9a8565
Suryansh Singh <singhsuryansh2746@gmail.com>
2026-08-12 20:19:58 +0530

    axios fixed with jwt

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -4,24 +4,21 @@ const api = axios.create({
     baseURL: "http://localhost:5000/api",
 });
 
-api.interceptors.request.use(
-    (config) => {
 
-        const token = localStorage.getItem("token");
 
-        if (token) {
+api.interceptors.request.use((config) => {
 
-            config.headers.Authorization =
-                `Bearer ${token}`;
+    const token = localStorage.getItem("token");
 
-        }
+    if (token) {
 
-        return config;
-    },
+        config.headers.Authorization = `Bearer ${token}`;
 
-    (error) => {
-        return Promise.reject(error);
     }
-);
+
+    return config;
+
+});
+
 
 export default api;
