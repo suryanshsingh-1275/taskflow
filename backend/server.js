a2878e70c27398e2859b4ef6b17a8d8dbf14ca5b
Suryansh Singh <singhsuryansh2746@gmail.com>
2026-08-16 21:28:08 +0530

    updated server.jsso that creates server with socket.io

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,19 @@
 import express from "express";
+import http from "http";
 import cors from "cors";
 import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+import { Server } from "socket.io";
+import { createAdapter } from "@socket.io/redis-adapter";
 
 import connectDB from "./config/db.js";
+import { pubClient, subClient } from "./config/redis.js";
 
 import authRoutes from "./routes/authRoutes.js";
 import boardRoutes from "./routes/boardRoutes.js";
 import taskRoutes from "./routes/taskRoutes.js";
 import analyticsRoutes from "./routes/analyticsRoutes.js";
-
-
+import notificationRoutes from "./routes/notificationRoutes.js";
 
 dotenv.config();
 
@@ -20,13 +24,79 @@ app.use(express.json());
 
 connectDB();
 
-app.use("/api/auth",authRoutes);
-app.use("/api/boards",boardRoutes);
-app.use("/api/tasks",taskRoutes);
-app.use("/api/analytics",analyticsRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/boards", boardRoutes);
+app.use("/api/tasks", taskRoutes);
+app.use("/api/analytics", analyticsRoutes);
+app.use("/api/notifications", notificationRoutes);
+
+
+// SOCKET.IO SETUP
+
+// app.listen() normally creates a raw http.Server internally and
+// hides it from you. Socket.io needs direct access to that server
+
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+    },
+});
+
+
+// Wires Socket.io to Redis pub/sub.
+
+io.adapter(createAdapter(pubClient, subClient));
+
+
+io.use((socket, next) => {
+
+    const token = socket.handshake.auth?.token;
+
+    if (!token) {
+        return next(new Error("Authentication required"));
+    }
+
+    try {
+
+        const decoded = jwt.verify(
+            token,
+            process.env.JWT_SECRET
+        );
+
+        socket.userId = decoded.userId;
+
+        next();
+
+    } catch (error) {
+
+        next(new Error("Invalid token"));
+
+    }
+
+});
+
+
+io.on("connection", (socket) => {
+
+    console.log("Socket connected:", socket.userId);
+
+    socket.join(socket.userId);
+
+    socket.on("disconnect", () => {
+        console.log("Socket disconnected:", socket.userId);
+    });
+
+});
+
+
+app.set("io", io);
+
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=>{
-    console.log("Server Running ");
+server.listen(PORT, () => {
+    console.log("Server Running");
 });
